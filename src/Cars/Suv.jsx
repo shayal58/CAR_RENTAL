476664fcc9d3c5c002727ce7ca8d1544f0c7d67c
shayal58 <117502747+shayal58@users.noreply.github.com>
2025-01-11 18:28:16 +0545

    Update Suv.jsx

--- src/Cars/Suv.jsx
+++ src/Cars/Suv.jsx
@@ -3,24 +3,24 @@ import * as Images from "../assets/image"
 import { Link } from 'react-router-dom'
 const Suv = () => {
   return (
-       <div className='ml-4 mr-4 mb-20 text-gre'>
-         <div className='flex mt-12 justify-evenly'>
-         <Link to ='/Suv/:userId'className='bg-secondary rounded-lg w-96 h-76'>  <button ><img src={Images.Normalcar1} width={300} height={400}></img></button><div className='flex  border-t-2 border-gre'><div>$100</div> <div className='ml-40 flex '>4 seater</div></div></Link>
-         <Link to='/Suv/:userId' className='bg-secondary rounded-lg w-96 h-76'>  <button ><img src={Images.Normalcar2} width={300} height={400}></img></button><div className='flex border-t-2 border-gre '><div>$100</div> <div className='ml-40'>4 seater</div></div></Link >
-         <Link to='/Suv/:userId' className='bg-secondary rounded-lg w-96 h-76'>  <button ><img src={Images.Normalcar3} width={400} height={400}></img></button><div className='flex mt-10 border-t-2 border-gre'><div>$100</div> <div className='ml-40'>4 seater</div></div></Link >
-         </div>
-         <div className='flex mt-12 justify-evenly'>
-         <Link to='/Suv/:userId' className='bg-secondary rounded-lg w-96 h-76'>  <button ><img src={Images.Normalcar1} width={300} height={400}></img></button><div className='flex border-t-2 border-gre '><div>$100</div> <div className='ml-40'>4 seater</div></div></Link >
-         <Link to='/Suv/:userId' className='bg-secondary rounded-lg w-96 h-76'>  <button ><img src={Images.Normalcar2} width={300} height={400}></img></button><div className='flex border-t-2 border-gre'><div>$100</div> <div className='ml-40'>4 seater</div></div></Link >
-         <Link to='/Suv/:userId' className='bg-secondary rounded-lg w-96 h-76'>  <button ><img src={Images.Normalcar3} width={400} height={400}></img></button><div className='flex mt-10 border-t-2 border-gre'><div>$100</div> <div className='ml-40'>4 seater</div></div></Link >
-         </div>
-         <div className='flex mt-12 justify-evenly'>
-         <Link to='/Suv/:userId' className='bg-secondary rounded-lg w-96 h-76'>  <button ><img src={Images.Normalcar1} width={300} height={400}></img></button><div className='flex border-t-2 border-gre '><div>$100</div> <div className='ml-40'>4 seater</div></div></Link >
-         <Link to='/Suv/:userId' className='bg-secondary rounded-lg w-96 h-76'>  <button ><img src={Images.Normalcar2} width={300} height={400}></img></button><div className='flex  border-t-2 border-gre'><div>$100</div> <div className='ml-40'>4 seater</div></div></Link >
-         <Link to='/Suv/:userId' className='bg-secondary rounded-lg w-96 h-76'>  <button ><img src={Images.Normalcar3} width={400} height={400}></img></button><div className='flex  mt-10 border-t-2 border-gre'><div>$100</div> <div className='ml-40'>4 seater</div></div></Link >
-         </div>
-   
-         </div>
+        <div className='ml-4 mr-4 md:mb-20 text-gre'>
+          <div className='flex md:mt-12 mt-8 justify-evenly'>
+          <Link to ='/Suv/:userId'className='bg-secondary rounded-lg md:w-96 w-[120px] md:h-72 h-40'>  <button ><img src={Images.Normalcar1} width={300} height={400}></img></button><div className='flex md:flex-row flex-col  border-t-2 border-gre'><div>$100</div> <div className='md:md:ml-40 flex   '>4 seater</div></div></Link>
+          <Link to='/Suv/:userId' className='bg-secondary rounded-lg md:w-96 w-[120px] md:h-72 h-40'>  <button ><img src={Images.Normalcar2} width={300} height={400}></img></button><div className='flex  md:flex-row flex-col border-t-2 border-gre '><div>$100</div> <div className='md:ml-40 flex '>4 seater</div></div></Link >
+          <Link to='/Suv/:userId' className='bg-secondary rounded-lg md:w-96 w-[120px] md:h-72 h-40'>  <button ><img src={Images.Normalcar3} width={400} height={400}></img></button><div className='flex  md:flex-row flex-col mt-8 border-t-2 border-gre'><div>$100</div> <div className='md:ml-40 flex '>4 seater</div></div></Link >
+          </div>
+          <div className='flex mt-12 justify-evenly'>
+          <Link to='/Suv/:userId' className='bg-secondary rounded-lg md:w-96 w-[120px] md:h-72 h-40'>  <button ><img src={Images.Normalcar1} width={300} height={400}></img></button><div className='flex  md:flex-row flex-col border-t-2 border-gre '><div>$100</div> <div className='md:ml-40 flex '>4 seater</div></div></Link >
+          <Link to='/Suv/:userId' className='bg-secondary rounded-lg md:w-96 w-[120px] md:h-72 h-40'>  <button ><img src={Images.Normalcar2} width={300} height={400}></img></button><div className='flex  md:flex-row flex-col border-t-2 border-gre'><div>$100</div> <div className='md:ml-40 flex '>4 seater</div></div></Link >
+          <Link to='/Suv/:userId' className='bg-secondary rounded-lg md:w-96 w-[120px] md:h-72 h-40'>  <button ><img src={Images.Normalcar3} width={400} height={400}></img></button><div className='flex md:flex-row flex-col  mt-8 border-t-2 border-gre'><div>$100</div> <div className='md:ml-40 flex '>4 seater</div></div></Link >
+          </div>
+          <div className='flex mt-12 justify-evenly'>
+          <Link to='/Suv/:userId' className='bg-secondary rounded-lg md:w-96 w-[120px] md:h-72 h-40'>  <button ><img src={Images.Normalcar1} width={300} height={400}></img></button><div className='flex  md:flex-row flex-col border-t-2 border-gre '><div>$100</div> <div className='md:ml-40 flex '>4 seater</div></div></Link >
+          <Link to='/Suv/:userId' className='bg-secondary rounded-lg md:w-96 w-[120px] md:h-72 h-40'>  <button ><img src={Images.Normalcar2} width={300} height={400}></img></button><div className='flex  md:flex-row flex-col border-t-2 border-gre'><div>$100</div> <div className='md:ml-40 flex '>4 seater</div></div></Link >
+          <Link to='/Suv/:userId' className='bg-secondary rounded-lg md:w-96 w-[120px] md:h-72 h-40'>  <button ><img src={Images.Normalcar3} width={400} height={400}></img></button><div className='flex  md:flex-row flex-col  mt-8 border-t-2 border-gre'><div>$100</div> <div className='md:ml-40 flex '>4 seater</div></div></Link >
+          </div>
+    
+          </div>
   )
 }
 
